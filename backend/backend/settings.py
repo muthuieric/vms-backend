73a010c17f5c8288d24f7bd4934889e0a728f000
Eric <ericmuthuipatch22@gmail.com>
2024-05-23 17:45:49 +0300

    changed database to sqlite3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -105,22 +105,22 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": 'postgres',
-        "USER": 'postgres',
-        "PASSWORD":'p123@sqd',
-        "HOST":'vms-database.cdq2uik26qke.us-east-1.rds.amazonaws.com',
-        "PORT": 5432
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": 'postgres',
+#         "USER": 'postgres',
+#         "PASSWORD":'p123@sqd',
+#         "HOST":'vms-database.cdq2uik26qke.us-east-1.rds.amazonaws.com',
+#         "PORT": 5432
+#     }
+# }
 
 
 # database_url = os.environ.get("DATABASE_URL")
